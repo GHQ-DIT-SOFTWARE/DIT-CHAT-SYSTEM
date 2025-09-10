0add9d6da50ebfed1194c17796d89ee03f3b0b12
360konet <akrobom@gmail.com>
2025-09-10 14:36:46 +0000

    New chat update

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,10 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+
+                    <x-nav-link :href="route('myChat')" :active="request()->routeIs('myChat')">
+                        {{ __('Chat Portal') }}
+                    </x-nav-link>
                 </div>
             </div>
 
